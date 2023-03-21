fefa90bb28d9f0b2156c95cab9eb6463e3af41f2
katzkb <katsuya7kubo@gmail.com>
2023-03-21 22:07:03 +0900

    Add a random initializer of tile

--- src/service/model/stage.ts
+++ src/service/model/stage.ts
@@ -1,10 +1,30 @@
 export class Stage {
-  tiles: number[][] = [][0]
+  size:           number     = 4
+  tiles:          number[][] = [][0]
+  initialTileNum: number     = 3
 
   constructor (size: number) {
     // 指定サイズで初期化
     this.tiles = Array.from(new Array(size), () => new Array(size).fill(0))
     // ランダム生成処理
+    this.size = size
+    this.setRandom(this.initialTileNum)
+  }
+
+  /**
+   * ランダムな座標に指定個数だけ初期タイルを設置する
+   * @param cnt 設置個数
+   */
+  private setRandom(cnt: number) {
+    for (let i = 0; i < cnt; i++) {
+      const x: number = Math.floor(Math.random() * this.size)
+      const y: number = Math.floor(Math.random() * this.size)
+      if (this.tiles[x][y] !== 0) {
+        i -= 1
+        continue
+      }
+      this.tiles[x][y] = 2
+    }
   }
 
   move() {
--- src/views/Stage.svelte
+++ src/views/Stage.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
-  import Tile from "./Tile.svelte"
-  import { Stage } from "../service/model/stage"
-  const stage = new Stage(4)
+  import Tile        from "./Tile.svelte"
+  import { Stage }   from "../service/model/stage"
+  import { browser } from "$app/environment"
+  let stage: Stage|undefined
+  if (browser) {
+    stage = new Stage(4)
+  }
 </script>
-<div id="stage">
-  {#each stage.tiles as tileLine , y}
-    {#each tileLine as tile, x}
-      <Tile x={x*50} y={y*50} n={tile}/>
+{#if stage !== undefined}
+  <div id="stage">
+    {#each stage.tiles as tileCol, x}
+      {#each tileCol as n, y}
+        <Tile x={x*50} y={y*50} n={n}/>
+      {/each}
     {/each}
-  {/each}
-</div>
+  </div>
+{/if}
 <style lang="scss">
   #stage {
     position: relative;
