ede5816eebaae2f84ee9ae9a952f0ad48dbe7f40
katzkb <katsuya7kubo@gmail.com>
2023-03-21 17:41:40 +0900

    Add components

---
+++ src/views/Stage.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Tile from "./Tile.svelte"
+</script>
+<div id="stage">
+  <Tile x=0   y=0/>
+  <Tile x=50  y=0/>
+  <Tile x=150 y=0/>
+</div>
+<style lang="scss">
+  #stage {
+    position: relative;
+    border: solid 1px black;
+    height: 200px;
+    width: 200px;
+  }
+</style>
---
+++ src/views/Tile.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let x: number
+  export let y: number
+</script>
+<div class="tile" style="left:{x}px; top:{y}px;">
+  <div class="number">2</div>
+</div>
+<style lang="scss">
+  .tile {
+    position: absolute;
+    display: flex;
+    box-sizing: border-box;
+    margin: 2px;
+    border: solid 1px black;
+    height: 46px;
+    width:  46px;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
